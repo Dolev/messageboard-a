90385d2c756fb7744f0104306f2ea43289de5cd1
Dolev <dolevhindy@gmail.com>
2021-03-23 17:27:10 +0200

    Final Code

--- Board.hpp
+++ Board.hpp
@@ -14,8 +14,6 @@ namespace ariel {
 			unsigned int column=1;
 			Direction direction=Direction::Horizontal;
 			vector<vector<string>> vec_board;
-
-			
 		public:
 		static void post(unsigned int row, unsigned int column, Direction direction,std::string const &message);
 		static std::string read(unsigned int row, unsigned int column, Direction direction, int length);
--- Test.cpp
+++ Test.cpp
@@ -21,17 +21,17 @@ TEST_CASE("just a test") {
     CHECK_NOTHROW(board.post(95,198,Direction::Vertical,"___"));
     CHECK_NOTHROW(board.post(95,198,Direction::Horizontal,"___"));
     //10
-    CHECK_NOTHROW(board.post(55,50,Direction::Vertical,"_____"));
-    CHECK_NOTHROW(board.post(100,200,Direction::Horizontal,"abc"));// should return - abcd (overwrite the same)
-    CHECK_NOTHROW(board.post(99,202,Direction::Vertical,"xyz"));
-    CHECK_NOTHROW(board.post(95,198,Direction::Horizontal,"___"));
-    CHECK_NOTHROW(board.post(95,198,Direction::Vertical,"___"));
+    CHECK_NOTHROW(board.post(30,40,Direction::Vertical," ")); //change to "_" -> " "
+    CHECK_NOTHROW(board.post(0,0,Direction::Horizontal,"abc"));// should return - abcd (overwrite the same)
+    CHECK_NOTHROW(board.post(99,2,Direction::Vertical,"yu"));
+    CHECK_NOTHROW(board.post(95,191,Direction::Horizontal," "));
+    CHECK_NOTHROW(board.post(95,138,Direction::Vertical,"___"));
     //15
-    CHECK_NOTHROW(board.post(55,50,Direction::Vertical,"_____"));
+    CHECK_NOTHROW(board.post(300,0,Direction::Vertical,"sd_dfgd"));
     CHECK_NOTHROW(board.post(100,200,Direction::Horizontal,"abc"));// should return - abcd (overwrite the same)
-    CHECK_NOTHROW(board.post(99,202,Direction::Vertical,"xyz"));
-    CHECK_NOTHROW(board.post(95,198,Direction::Horizontal,"___"));
-    CHECK_NOTHROW(board.post(95,198,Direction::Vertical,"___"));
+    CHECK_NOTHROW(board.post(2,0,Direction::Vertical,"xdsadsadas"));
+    CHECK_NOTHROW(board.post(85,57,Direction::Horizontal,"ewewt"));
+    CHECK_NOTHROW(board.post(0,300,Direction::Vertical,"saddas"));
     //20
     /* Add more checks here */
 }
