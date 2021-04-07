ccdbe73a396c608a4afe4c9b6bdce605739033d9
Dolev .H <33119992+Dolev@users.noreply.github.com>
2021-04-07 19:53:39 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,18 +1,81 @@
 #include "Board.hpp"
-/*
-printf("Number of rows: %d\n", LEN(arr));
-printf("Number of columns: %d\n", LEN(arr[0]));
-			*/
+#include<map>
+#include <iostream>
+using namespace std;
+
 namespace ariel {
-    void Board::post(unsigned int row, unsigned int column, Direction direction,std::string const &message) {
+     void Board::post(unsigned int row, unsigned column, Direction direction,string const &message) {
+         //insert my post 
+        if(direction==Direction::Vertical){
+        NumRow=message.length();
+        NumColumn++;
+         for(unsigned int i=0;i<message.length();i++){
+                 MyBoard[row][column]=message.at(i);
+                 row++;
+            }
+            }
+                  if(direction==Direction::Horizontal){
+            NumColumn=message.length();
+            NumRow++;
+            for(unsigned int i=0;i<message.length();i++){
+              
+                 MyBoard[row][column]=message.at(i);
+                 column++;
+            }
+            }
 
+         }
+     
 
+    string Board::read(unsigned int row, unsigned int column, Direction direction,unsigned int length) {
+           string result;
+           char output=0;
+                            if(direction==Direction::Horizontal){
 
+           for (unsigned int i = 0; i < length; i++) {  
+                       if(MyBoard[row][column]==0){
+                           result+= '_';
+                           column++;
+             }
+             else{
+                     output=MyBoard[row][column];
+                     result+=output;
+                     column++;
+
+                 }
+                 }
+                            }
+
+          if(direction==Direction::Vertical){
+          for (unsigned int i = 0; i < length; i++) {  
+
+                if(MyBoard[row][column]==0){
+                   result+= '_';
+                   row++;
+                }
+                else{
+                     output=MyBoard[row][column];
+                     result+=output;
+                     row++;
+                }
+
+                 }
+               }
+            return result;
     }
-    std::string Board::read(unsigned int row, unsigned int column, Direction direction, int length) {
-	        return "";
-    }
-    void Board::show() {
 
+     void Board::show() {
+         
+	 //unsigned int lastRow = NumRow; //
+    for (unsigned i = 0; i < NumRow; i++)
+    {
+        for (unsigned j = 0; j < NumColumn; j++)
+        {
+            cout<<MyBoard[i][j];
+        }   
+        cout<< endl;
     }
+    
+	}
 }
+
